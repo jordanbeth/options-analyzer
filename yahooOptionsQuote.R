59d45cf51032e67f640cb0cfdbf5d34e490c4147
jordanbeth <jordanbeth14@gmail.com>
2017-05-01 16:24:23 -0400

    changed up function calls to make them more thorough. Goit more data on calls for the whole year

--- yahooOptionsQuote.R
+++ yahooOptionsQuote.R
@@ -1,12 +1,29 @@
 library(RCurl)
 library(XML)
 library(jsonlite)
-library(plot3D)
 library(akima)
 library(zoo)
+library(lubridate)
+
 # wrapper to run everything on command
 optionsRunner <- function(symbol="AAPL"){
+
+  #######  
+  scrapeExpiries <- function(symbol){
+    baseUrl <- "https://finance.yahoo.com/quote/"
+    
+    url <- paste(baseUrl, symbol, "/options?p=", symbol, sep="")
+    # pull the html from the given url
+    htmlString <- getURL(url)
+    # parse the html string 
+    htmlParsed <- htmlParse(htmlString, asText = TRUE) 
+    # locate the expiries using XPath
+    value.expiries <- xpathSApply(htmlParsed, "//div[@class = 'Fl(start) Pend(18px) option-contract-control drop-down-selector']//option", xmlGetAttr, "value")
+    
+    return(value.expiries)
+  }
   
+  #######
   getOptions <- function(symbol, date){
     baseUrl <- "https://query2.finance.yahoo.com/v7/finance/options/"
     
@@ -22,88 +39,120 @@ optionsRunner <- function(symbol="AAPL"){
     
     # return the types of options separately
     lst <- list(jsonCalls, jsonPuts)
+    
     return(lst)
     
   }
   
-  #####
-  # manually getting options quotes passing a ticker symbol and an Epoch date
-  symbol <- symbol 
-  may5 <- getOptions(symbol, "1493942400")
-  may12 <- getOptions(symbol, "1494547200")
-  may19 <- getOptions(symbol, "1495152000")
-  may26 <- getOptions(symbol, "1495756800")
-  
-  #####
+  #######
   # function to extract the necessary information from the calls data frame
   formatCalls <- function(calls){
-    concatCalls <- cbind(calls[[1]]$strike, calls[[1]]$expiration, calls[[1]]$lastPrice)
+    concatCalls <- list()
+    
+    for(callExp in calls){
+      relevantInfo <- cbind(callExp$strike, callExp$expiration, callExp$lastPrice)
+      concatCalls <- rbind(concatCalls, relevantInfo)
+    }
+    
     callsData <- as.data.frame(concatCalls)
     names(callsData) <- c("Strike", "Days to Expiration", "Price")
     return(callsData)
+      
   }
   
-  ####
+  #######
   # function to extract the necessary information from the puts data frame
   formatPuts <- function(puts){
-    puts <- cbind(puts[[2]]$strike, puts[[2]]$expiration, puts[[2]]$lastPrice)
-    puts <- as.data.frame(puts)
-    names(puts) <- c("Strike", "Days to Expiration", "Price")
-    return(puts)
+    concatPuts <- list()
+    
+    for(putExp in puts){
+      relevantInfo <- cbind(putExp$strike, putExp$expiration, putExp$lastPrice)
+      concatPuts <- rbind(concatPuts, relevantInfo)
+    }
+    
+    putsData <- as.data.frame(concatPuts)
+    names(putsData) <- c("Strike", "Days to Expiration", "Price")
+    return(putsData)
   }
   
-  #### 
-  may5calls <- formatCalls(may5)
-  may5puts <- formatPuts(may5)
+  ####### 
+  # *Invoked on call so that the function can take related action
+  initializer <- function(){
+    
+    # getting the dates from the html of the website and turning them into strings
+    dates <- scrapeExpiries(symbol)
+    calls <- list()
+    puts <- list()
   
-  may12calls <- formatCalls(may12)
-  may12puts <- formatPuts(may12)
+    # Iterating through the expiries to get their corresponding options
+    for(expiry in dates){
+      optionsInstance <- getOptions(symbol, expiry)
+    
+      calls <- append(calls, optionsInstance[1])
+      puts <- append(puts, optionsInstance[2])
+    }
+    
+    # assigning the formatted result to the appropriate variable
+    calls <- formatCalls(calls)
+    puts <- formatPuts(puts)
+
+    ####
   
-  may19calls <- formatCalls(may19)
-  may19puts <- formatPuts(may19)
+    # Time to expiry using time of expiry in Epoch minus the current time
+    timeToExpEpoch <- function(timeOfExp){
+      timeNow <- as.numeric(Sys.time())
+      (timeOfExp - timeNow) / (24*60*60)
+    }
+    print(calls)
+    # convert the Epoch time to expiry into Days
+    calls["Days to Expiration"] <- floor(sapply(calls["Days to Expiration"], function(x){timeToExpEpoch(as.numeric(x))}))
+    
+    # convert the Epoch time to expiry into Days
+    calls["Days to Expiration"] <- floor(sapply(calls["Days to Expiration"], timeToExpEpoch) / (24*60*60))
+    
+    return(calls)
   
-  may26calls <- formatCalls(may26)
-  may26puts <- formatPuts(may26)
-  ####
-  
-  # Time to expiry using time of expiry in Epoch minus the current time
-  timeToExpEpoch <- function(timeOfExp){
-    timeNow <- as.numeric(Sys.time())
-    timeOfExp - timeNow
   }
   
-  # bind all of the calls into one data frame
-  calls <- rbind(may5calls, may12calls, may19calls, may26calls)
-  
-  # convert the Epoch time to expiry into Days
-  calls["Days to Expiration"] <- floor(sapply(calls["Days to Expiration"], timeToExpEpoch) / (24*60*60))
-  
-  # bind all of the puts into one data frame
-  puts <- rbind(may5puts, may12puts, may19puts, may26puts)
-  
-  # convert the Epoch time to expiry into Days
-  puts["Days to Expiration"] <- floor(sapply(puts["Days to Expiration"], timeToExpEpoch) / (24*60*60))
-  
+  ##### Invoked on function call
+  initializer()
+}
+
+
+
+
+
+
+
+
+
   ####
-  calls <- interp(calls[,1], calls[,2], calls[,3])
-  interpolatedCalls <- interp2xyz(calls)
+  #calls <- interp(calls[,1], calls[,2], calls[,3], xo=seq(20, 215, 5), yo=seq(3, 20, 1), extrap=TRUE)
+  #interpolatedCalls <- interp2xyz(calls)
   
-  x <- as.matrix(interpolatedCalls[,1])
-  y <- as.matrix(interpolatedCalls[,2])
-  z <- as.matrix(interpolatedCalls[,3])
+  # approximate NA values in object
+  # interpolatedCallsNAApprox <- na.approx(interpolatedCalls)
+  # interpolatedCallsNAApprox[,3][is.na(interpolatedCallsNAApprox[,3])] <- 0
   
-  puts <- interp(puts[,1], puts[,2], puts[,3])
-  interpolatedPuts <- interp2xyz(puts)
+  #x <- as.matrix(interpolatedCallsNAApprox[,1])
+  #y <- as.matrix(interpolatedCallsNAApprox[,2])
+  #z <- as.matrix(interpolatedCallsNAApprox[,3])
   
-  callsJSON <- toJSON(calls)
-  putsJSON <- toJSON(puts)
+  # convert NA to 0 for graphing
+  #z[is.na(z)] <- 0
+  
+  #puts <- interp(puts[,1], puts[,2], puts[,3])
+  #interpolatedPuts <- interp2xyz(puts)
+  
+  #callsJSON <- toJSON(calls)
+  #putsJSON <- toJSON(puts)
 
   ####
   # write(callsJSON, file="calls.js")
   # write(putsJSON, file="puts.js")
-  write(callsJSON, file="test.js")
-  
-  return(calls)
-  # write.csv(calls, file = "mayCalls", row.names = FALSE)
+  # write(callsJSON, file="test.js"))
+  # write.csv(interpolatedCallsNAApprox, file = "mayCalls", row.names = FALSE)
   # write.csv(puts, file = "mayPuts", row.names = FALSE)
-}
+  #return(interpolatedCallsNAApprox)
+
+ 
